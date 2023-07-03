6e003632a8039dc9c121fcb215a6a34f400f15af
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-03 22:50:42 +0300

    Change screen

--- App.jsx
+++ App.jsx
@@ -15,8 +15,8 @@ export default function App() {
 
   return (
     <>
-      {/* <RegistrationScreen /> */}
-      <LoginScreen />
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
     </>
   );
 }
