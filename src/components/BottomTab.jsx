669849da7d4398ec295f0e3def21d8d33efc04c4
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-09 15:02:46 +0300

    Feat(JSX) Added bottom components

---
+++ src/components/BottomTab.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { TouchableOpacity, View } from 'react-native';
+
+import PostsScreen from '../Screens/PostsScreen';
+import CreatePostsScreen from '../Screens/CreatePostsScreen';
+import ProfileScreen from '../Screens/ProfileScreen';
+
+import { useNavigation } from '@react-navigation/native';
+
+import Left from '../image/left.svg';
+import Grid from '../image/grid.svg';
+import User from '../image/user.svg';
+import Plus from '../image/plus.svg';
+import Logout from '../image/logout.svg';
+
+const BottomTab = createBottomTabNavigator();
+
+export const BottomMenu = () => {
+  const navigation = useNavigation();
+
+  return (
+    <BottomTab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: { height: 80, justifyContent: 'center' },
+        headerTitleAlign: 'center',
+
+        headerRightContainerStyle: { paddingRight: 16, paddingBottom: 9 },
+        headerLeftContainerStyle: { paddingLeft: 16, paddingBottom: 9 },
+      }}
+    >
+      <BottomTab.Screen
+        name="PostsScreen"
+        component={PostsScreen}
+        options={{
+          headerShown: true,
+          tabBarIcon: ({ focused, size, color }) => (
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: focused ? '#FF6C00' : '#FFFFFF',
+                width: 70,
+                height: 40,
+                borderRadius: 20,
+              }}
+            >
+              <Grid
+                size={size}
+                color={color}
+                strokeOpacity={0.8}
+                stroke={focused ? '#FFFFFF' : '#212121'}
+              />
+            </View>
+          ),
+          headerRight: ({ focused, size, color }) => (
+            <TouchableOpacity>
+              <Logout size={size} color={color} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="CreatePostsScreen"
+        component={CreatePostsScreen}
+        options={{
+          tabBarStyle: { display: 'none' },
+          tabBarIcon: ({ focused, size, color }) => (
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: focused ? '#FF6C00' : '#FFFFFF',
+                width: 70,
+                height: 40,
+                borderRadius: 20,
+              }}
+            >
+              <Plus
+                size={size}
+                color={color}
+                fillOpacity={0.8}
+                fill={focused ? '#FFFFFF' : '#212121'}
+              />
+            </View>
+          ),
+          headerLeft: ({ focused, size, color }) => (
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Left size={size} color={color} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="ProfileScreen"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, size, color }) => (
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: focused ? '#FF6C00' : '#FFFFFF',
+                width: 70,
+                height: 40,
+                borderRadius: 20,
+              }}
+            >
+              <User
+                size={size}
+                color={color}
+                stroke={focused ? '#FFFFFF' : '#212121'}
+              />
+            </View>
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+};
