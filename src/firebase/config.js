8084794624c8723d3fd2a3478d3ab94ee57566fa
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-09-10 20:59:55 +0300

    Test

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,14 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+  apiKey: 'AIzaSyCwJs8iRrzY1QK5mLSomMXaj2bhHUyLPv4',
+  authDomain: 'react-native-hw-9e922.firebaseapp.com',
+  databaseURL: 'https://react-native-hw-9e922-default-rtdb.firebaseio.com',
+  projectId: 'react-native-hw-9e922',
+  storageBucket: 'react-native-hw-9e922.appspot.com',
+  messagingSenderId: '699795428995',
+  appId: '1:699795428995:web:31929227f5b4399b376db6',
 };
 
 const app = initializeApp(firebaseConfig);
